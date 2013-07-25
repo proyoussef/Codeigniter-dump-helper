3894a7c7f8aa18965000e3ce4e73a33aaa31b437
proyoussef <jlidat@gmail.com>
2013-07-25 10:50:12 +0000

    Update dump_helper.php

--- dump_helper.php
+++ dump_helper.php
@@ -5,3 +5,28 @@
  * @author Youssef JLIDAT (jlidat@gmail.com)
  * @version 1.0
  */
+ 
+if (!function_exists('dump')) {
+    function dump ($var, $show = TRUE, $exit = FALSE) {
+
+        // Add formatting
+        $output = '<pre>' . $output . '</pre>';
+        
+        // Output
+        if ($show == TRUE) {
+            echo $output;
+        }
+        else {
+            return $output;
+        }
+        
+        //exit ?
+        if ($exit == TRUE) {
+            exit;
+        }
+        
+    }
+}
+
+/* End of file dump_helper.php */
+/* Location: ./application/helpers/dump_helper.php */
